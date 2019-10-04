6ca70475946f6304ec390f4c7f9185d60e72077c
Fran <fran.perez.c@hotmail.com>
2019-10-04 17:52:24 -0300

    initial commit

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,3 +1,18 @@
 import React from "react"
 
-export default () => <div>Hello world!</div>
+// Estilos
+import 'normalize.css';
+import '../styles/base.scss';
+
+// Componentes
+import Portada from '../components/portada';
+
+const Index = () => {
+    return (
+        <div>
+            <Portada/>
+        </div>
+    )
+}
+
+export default Index;
