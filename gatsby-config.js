e7948db989f5f2bbbcb1ee51e6c3a0054e444062
Fran <fran.perez.c@hotmail.com>
2019-10-04 23:55:24 -0300

    responsive

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,5 +1,24 @@
 module.exports = {
 	plugins: [
-		'gatsby-plugin-sass'
+		'gatsby-plugin-sass',
+		{
+			resolve: `gatsby-plugin-prefetch-google-fonts`,
+			options: {
+				fonts: [
+					{
+						family: `Lora`,
+						variants: [`400`, `400i`, `700`, `700i`]
+					},
+					{
+						family: `Merriweather`,
+						variants: [`400`, `700i`]
+					},
+					{
+						family: `Open Sans`,
+						variants: [`400`, `400i`, `700`, `700i`]
+					}
+				],
+			},
+		}
 	]
 }
