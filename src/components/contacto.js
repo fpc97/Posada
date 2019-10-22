6787a5883aebde8e40212d18718a28cde794ee95
Fran <fran.perez.c@hotmail.com>
2019-10-22 11:52:23 -0300

    terminado casi

---
+++ src/components/contacto.js
@@ -0,0 +1,105 @@
+import React from 'react';
+
+// Estilos
+import contactoStyle from '../styles/contacto.module.scss';
+
+const Contacto = () => {
+    return (
+        <footer className={contactoStyle.footer}>
+            <div className={contactoStyle.color}>
+                <h2 className={contactoStyle.titulo}>Contacto</h2>
+                <div className={contactoStyle.wrapperContenido}>
+                    <address className={contactoStyle.direcciones}>
+                        <p className={contactoStyle.consultasReservas}>Consultas y reservas: <br></br><span>María Elena Compagnucci de Carreño</span></p>
+                        <ul className={contactoStyle.direcUl}>
+                            <li className={contactoStyle.direcLi}>
+                                <div className={contactoStyle.direcKey}>
+                                    <svg className={contactoStyle.direcIcon} xmlns="http://www.w3.org/2000/svg" viewBox="0 0 128 96"><path d="M1 17v96H129V17ZM115.8 27.7 65 68.8 14.2 27.7ZM11.7 102.3v-63L65 82.5l53.3-43.2v63Z" transform="translate(-1 -17)"/></svg>
+                                </div>
+                                <div className={contactoStyle.direcValue}>
+                                    <a className={contactoStyle.direcLink} href='mailto:posadabyb@yahoo.com.ar'>posadabyb<wbr></wbr>@yahoo.com.ar</a>
+                                </div>
+                            </li>
+                            <li className={contactoStyle.direcLi}>
+                                <div className={contactoStyle.direcKey}>
+                                    <svg className={contactoStyle.direcIcon} xmlns="http://www.w3.org/2000/svg" viewBox="0 0 70.7 106"><path d="M100.3 111.9l-15.5-30s-8.7 4.3-9.1 4.5c-9.9 4.8-30-34.5-20.4-39.7l9.2-4.5L49.1 12l-9.3 4.6C8 33.2 58.5 131.3 91 116.5 91.6 116.2 100.3 111.9 100.3 111.9Z" transform="translate(-29.67 -12)"/></svg>
+                                </div>
+                                <div className={contactoStyle.direcValue}>
+                                    <a className={contactoStyle.direcLink} href="tel:+54-3547-421811">+54-3547-421811</a>
+                                    <br></br>
+                                    <a className={contactoStyle.direcLink} href="tel:+54-9-3547-15-578716">+54-9-3547-15-578716</a>
+                                </div>
+                            </li>
+                            <li className={contactoStyle.direcLi}>
+                                <div className={contactoStyle.direcKey}>
+                                    <svg className={contactoStyle.direcIcon} xmlns="http://www.w3.org/2000/svg" viewBox="0 0 70.7 106"><path d="M65 12C46.5 12 29.7 27 29.7 45.6S45 86.3 65 118c20-31.7 35.3-53.9 35.3-72.4S83.6 12 65 12Zm0 48.6A13.3 13.3 0 1 1 78.3 47.3 13.3 13.3 0 0 1 65 60.6Z" transform="translate(-29.67 -12)"/></svg>
+                                </div>
+                                <div className={contactoStyle.direcValue}>
+                                    <span className={contactoStyle.direcLink}>Eduardo Madero 339 - Alta Gracia - 5186 - Córdoba, Argentina</span>
+                                </div>
+                            </li>
+                        </ul>
+                    </address>
+                    <div className={contactoStyle.mail}>
+                        <h3 className={contactoStyle.mailTitulo}>Enviar mail</h3>
+                        <form className={contactoStyle.form}>
+                            <div className={contactoStyle.formCampo}>
+                                <label className={contactoStyle.formLabel}>Nombre *</label>
+                                <div className={contactoStyle.formInputTextWrap}>
+                                    <input className={contactoStyle.formInputText} type='text'></input>
+                                </div>
+                            </div>
+                            <div className={contactoStyle.formCampo}>
+                                <label className={contactoStyle.formLabel}>Teléfono *</label>
+                                <div className={contactoStyle.formInputTextWrap}>
+                                    <input className={contactoStyle.formInputText} type='text'></input>
+                                </div>
+                            </div>
+                            <div className={contactoStyle.formCampo}>
+                                <label className={contactoStyle.formLabel}>E-mail *</label>
+                                <div className={contactoStyle.formInputTextWrap}>
+                                    <input className={contactoStyle.formInputText} type='text'></input>
+                                </div>
+                            </div>
+                            <div className={contactoStyle.formCampo + ' ' + contactoStyle.largoMedio + ' ' + contactoStyle.nullMargin}>
+                                <label className={contactoStyle.formLabel}>País</label>
+                                <div className={contactoStyle.formInputTextWrap}>
+                                    <input className={contactoStyle.formInputText} type='text'></input>
+                                </div>
+                            </div>
+                            <div className={contactoStyle.formCampo + ' ' + contactoStyle.largoMedio}>
+                                <label className={contactoStyle.formLabel}>Provincia</label>
+                                <div className={contactoStyle.formInputTextWrap}>
+                                    <input className={contactoStyle.formInputText} type='text'></input>
+                                </div>
+                            </div>
+                            <div className={contactoStyle.formCampo + ' ' + contactoStyle.largoMedio + ' ' + contactoStyle.nullMargin}>
+                                <label className={contactoStyle.formLabel}>Ciudad</label>
+                                <div className={contactoStyle.formInputTextWrap}>
+                                    <input className={contactoStyle.formInputText} type='text'></input>
+                                </div>
+                            </div>
+                            <div className={contactoStyle.formCampo + ' ' + contactoStyle.largoMedio}>
+                                <label className={contactoStyle.formLabel}>Domicilio</label>
+                                <div className={contactoStyle.formInputTextWrap}>
+                                    <input className={contactoStyle.formInputText} type='text'></input>
+                                </div>
+                            </div>
+                            <div className={contactoStyle.formCampo}>
+                                <label className={contactoStyle.formLabel}>Consulta *</label>
+                                <div className={contactoStyle.formInputTextWrap}>
+                                    <textarea className={contactoStyle.formInputText + ' ' + contactoStyle.formTextarea}></textarea>
+                                </div>
+                            </div>
+                            <div className={contactoStyle.formSubmitWrap}>
+                                <button className={contactoStyle.formSubmit}>Enviar</button>
+                            </div>
+                        </form>
+                    </div>
+                </div>
+            </div>
+        </footer>
+    )
+};
+
+export default Contacto;
--- src/components/mapa-leaflet backup.js
+++ src/components/mapa-leaflet backup.js
@@ -1,29 +1,23 @@
 import React from 'react';
 
 // Librerias
-//import L from 'leaflet';
-//import 'leaflet-routing-machine';
+import L from 'leaflet';
+import 'leaflet-routing-machine';
 
 // Estilos
 import ubicacionStyle from '../styles/ubicacion.module.scss';
 import '../styles/borrarDescripcion.scss';
 
-class Mapa extends React.Component {
+class Map extends React.Component {
     state = {
-        rutaActual: null,
-        L: null,
-        rm: null
+        rutaActual: null
     }
 
     establecerRuta(num) {
-        const fallbL = this.state.L || require('leaflet'),
-            rm = this.state.rm || require('leaflet-routing-machine');
-
-
         const puntoOriginal = [-31.6576608, -64.4395798],
             puntoMedio = [puntoOriginal[0] + (num[0] - puntoOriginal[0]) / 2, puntoOriginal[1] + (num[1] - puntoOriginal[1]) / 2];
 
-        const iconoInvisible = fallbL.icon({
+        const iconoInvisible = L.icon({
             iconUrl: require('../img/invisible.png'),
             shadowUrl: require('../img/invisible.png'),
         
@@ -34,7 +28,7 @@ class Mapa extends React.Component {
             popupAnchor:  [1, 1]            // point from which the popup should open relative to the iconAnchor
         });
         
-        const iconoDestino = fallbL.icon({
+        const iconoDestino = L.icon({
             iconUrl: require('../img/marcador-destino.png'),
             shadowUrl: require('../img/marcador-destino-sombra.png'),
         
@@ -45,10 +39,10 @@ class Mapa extends React.Component {
             popupAnchor:  [22, 52]          // point from which the popup should open relative to the iconAnchor
         });
 
-        const ruta = fallbL.Routing.control({
+        const ruta = L.Routing.control({
             waypoints: [
-                fallbL.latLng(-31.6576608, -64.4395798),
-                fallbL.latLng(num[0], num[1])
+                L.latLng(-31.6576608, -64.4395798),
+                L.latLng(num[0], num[1])
             ],
             lineOptions: {
                 styles: [
@@ -68,7 +62,7 @@ class Mapa extends React.Component {
                 } else if (i == n - 1) {
                     marker_icon = iconoDestino;
                 }
-                var marker = fallbL.marker (start.latLng, {
+                var marker = L.marker (start.latLng, {
                     icon: marker_icon
                 });
                 return marker
@@ -82,62 +76,54 @@ class Mapa extends React.Component {
         this.map.panTo(puntoMedio);
     }
 
-    crearMapa() {
-        if (typeof window !== 'undefined') {
-            const L = require('leaflet');
-            const rm = require('leaflet-routing-machine');
-
-            if (typeof this.state.L === 'undefined') {
-                this.setState({
-                    L: L,
-                    rm: rm
-                });
-            }
-            // create map
-            this.map = L.map('mapolo', {
-                center: [-31.6563844, -64.4396453],
-                zoom: 17,
-                layers: [
-                    L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png', {
-                        attribution: '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
-                    })
-                ]
-            });
-            
-            const iconoCasa = L.icon({
-                iconUrl: require('../img/marcador-posada.png'),
-                shadowUrl: require('../img/marcador-posada-sombra.png'),
-            
-                iconSize:     [70, 70],     // size of the icon
-                shadowSize:   [100, 70],    // size of the shadow
-                iconAnchor:   [34, 70],     // point of the icon which will correspond to marker's location
-                shadowAnchor: [30, 53],     // the same for the shadow
-                popupAnchor:  [34, 70]      // point from which the popup should open relative to the iconAnchor
-            });
-    
-            this.marker = L.marker([-31.6576608, -64.4395798], {icon: iconoCasa}).addTo(this.map);
-        }
-    }
-
     componentDidMount() {
-        this.crearMapa();
+        // create map
+        this.map = L.map('map', {
+            center: [-31.6563844, -64.4396453],
+            zoom: 17,
+            layers: [
+                L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png', {
+                    attribution: '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+                })
+            ]
+        });
+        
+        const iconoCasa = L.icon({
+            iconUrl: require('../img/marcador-posada.png'),
+            shadowUrl: require('../img/marcador-posada-sombra.png'),
+        
+            iconSize:     [70, 70],     // size of the icon
+            shadowSize:   [100, 70],    // size of the shadow
+            iconAnchor:   [34, 70],     // point of the icon which will correspond to marker's location
+            shadowAnchor: [30, 53],     // the same for the shadow
+            popupAnchor:  [34, 70]      // point from which the popup should open relative to the iconAnchor
+        });
+
+        this.marker = L.marker([-31.6576608, -64.4395798], {icon: iconoCasa}).addTo(this.map);
     }
 
     componentWillReceiveProps(newProps) {
-        if (typeof this.map !== undefined) {
-            console.log(newProps.destino);
-            newProps.destino===null||this.establecerRuta(newProps.destino);
-        }
+        console.log(newProps.destino);
+        newProps.destino===null||this.establecerRuta(newProps.destino);
     }
 
     render() {
+        if (typeof window !== 'undefined') {
+            return (
+                <div>
+                    <link rel="stylesheet" href="https://unpkg.com/leaflet@1.5.1/dist/leaflet.css" integrity="sha512-xwE/Az9zrjBIphAcBb3F6JVqxf46+CDLwfLMHloNu6KEQCAWi6HcDUbeOfBIptF7tcCzusKFjFw2yuvEpDL9wQ==" crossorigin=""/>
+                    <div id="map" className={`container-imagen ${ubicacionStyle.mapa}`}></div>
+                </div>
+            )
+        }
+        /*
         return (
             <div>
                 <link rel="stylesheet" href="https://unpkg.com/leaflet@1.5.1/dist/leaflet.css" integrity="sha512-xwE/Az9zrjBIphAcBb3F6JVqxf46+CDLwfLMHloNu6KEQCAWi6HcDUbeOfBIptF7tcCzusKFjFw2yuvEpDL9wQ==" crossorigin=""/>
-                <div id="mapolo" className={`container-imagen ${ubicacionStyle.mapa}`}></div>
+                <div id="map" className={`container-imagen ${ubicacionStyle.mapa}`}></div>
             </div>
-        );
+        )*/
     }
 }
 
-export default Mapa;
+export default Map;
